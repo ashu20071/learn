7307072474553c1abddef5a73e5366c57fc4fd00
ashutosh.rk.singh <ashu20071@gmail.com>
2021-01-09 13:09:27 +0530

    LCA

---
+++ src/Tree/LowestCommonAncestor.java
@@ -0,0 +1,45 @@
+package Tree;
+
+public class LowestCommonAncestor {
+    //returns lowest common ancestor in a binary tree
+    TreeNode answer;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (exists(p.left, q.data) || exists(p.right, q.data))
+            return p;
+        if (exists(q.left, p.data) || exists(q.right, p.data))
+            return q;
+        check(root, p, q);
+        return answer;
+    }
+
+    public void check(TreeNode curr, TreeNode p, TreeNode q) {
+        if (curr == null)
+            return;
+
+        if ((exists(curr.left, p.data) && exists(curr.right, q.data)) ||
+                exists(curr.left, q.data) && exists(curr.right, p.data)) {
+            answer = curr;
+            return;
+        }
+        check(curr.left, p, q);
+        check(curr.right, p, q);
+    }
+
+    public boolean exists(TreeNode root, int value) {
+        if (root == null)
+            return false;
+        if (root.data == value)
+            return true;
+        return exists(root.left, value) || exists(root.right, value);
+    }
+
+    public static void main(String[] args) {
+        buildTree tree = new buildTree();
+        TreeNode root = tree.build("3 5 1 6 2 0 8 N N 7 4");
+        LowestCommonAncestor lca = new LowestCommonAncestor();
+        TreeNode p = root.left;
+        TreeNode q = root.left.right.right;
+        TreeNode lowest = lca.lowestCommonAncestor(root, p, q);
+        System.out.println(lowest.data);
+    }
+}
